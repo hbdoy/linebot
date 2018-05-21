cc6b4e1c973556eb381cf21d173a274828bf9ff6
hbdoy <felicity860128@gmail.com>
2018-05-21 16:45:47 +0800

    key in server

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ var linebot = require('linebot');
 var express = require('express');
 var request = require('request');
 var superagent = require('superagent');
+var firebase = require("firebase");
+
+// Initialize Firebase
+var config = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
+};
+firebase.initializeApp(config);
+var db = firebase.database();
+
+
 
 var bot = linebot({
     channelId: process.env.channelID,
@@ -54,21 +69,27 @@ function _botInit() {
             var msg = event.message.text;
             var replyMsg = '';
             var counter = '';
+            var action = '';
             if (msg == "功能") {
+                action = msg;
                 replyMsg = "抓: 查看最近熱門文章,\n抓週: 查看7天內熱門文章,\n抓月: 查看30天內熱門文章,\n說明: 查看說明,\n我是誰: 查看我是誰,\n誰最帥: 查看誰最帥,\n聯絡: 聯絡作者,\n滾: 嗚嗚...";
             } else if (msg == "說明") {
+                action = msg;
                 replyMsg = "沒有時間看靠北版?\n但又想知道最近大家再靠北什麼嗎?\n\n歡迎使用本機器人\n幫您統整近期/一週/一個月內的熱門文章\n(熱門文章: 透過演算法評量按讚、留言、分享數)\n\n用法: 直接輸入想使用的指令即可，ex: 「抓」\n\n這是閒暇之餘的作品\n部屬在免費空間\n沒有反應可以再輸入一次或是稍後再試\n當然也歡迎小額donate\n將會用在伺服器升級(應該啦)";
             } else if (msg == "我是誰") {
+                action = msg;
                 waitForAjax = true;
                 event.source.profile().then(function (profile) {
                     event.reply('Hello ' + profile.displayName);
-                    console.log(profile);
                 });
             } else if (msg == "聯絡") {
+                action = msg;
                 replyMsg = "開放許願功能，另外有任何問題都歡迎與我聯繫\ncowpei@protonmail.com";
             } else if (msg == "誰最帥") {
+                action = msg;
                 replyMsg = "李叡";
             } else if (msg == "抓") {
+                action = msg;
                 console.log(NCNUPosts.length);
                 replyMsg = [];
                 counter = "";
@@ -92,6 +113,7 @@ function _botInit() {
                     }
                 }
             } else if (msg == "抓週") {
+                action = msg;
                 console.log(NCNUPostsW.length);
                 replyMsg = [];
                 counter = "";
@@ -115,6 +137,7 @@ function _botInit() {
                     }
                 }
             } else if (msg == "抓月") {
+                action = msg;
                 console.log(NCNUPostsM.length);
                 replyMsg = [];
                 counter = "";
@@ -138,6 +161,7 @@ function _botInit() {
                     }
                 }
             } else if (msg == "滾") {
+                action = msg;
                 waitForAjax = true;
                 if (event.source.groupId) {
                     replyMsg = "我會再回來的";
@@ -168,19 +192,36 @@ function _botInit() {
                     console.log('error');
                 });
             }
+            event.source.profile().then(function (profile) {
+                pushUserData(profile);
+                if(action != ''){
+                    pushActionLog({userId: profile.userId, action: action});
+                }
+                if (event.source.groupId) {
+                    pushGroup(event.source.groupId);
+                } else if (event.source.roomId) {
+                    pushRoom(event.source.roomId);
+                }
+                console.log(profile);
+            });
         }
     });
     bot.on('join', function (event) {
-        event.source.member().then(function (member) {
-            console.log(member);
-        });
         event.reply("感謝您將本帳號加入群組，也歡迎將本帳號設為好友！\n\n每天都有好多的靠北文\n全部看完很花時間\n但又想知道最近哪些靠北文比較火紅嗎？\n\n歡迎使用本懶人靠北包\n不知道要怎麼操作?\n試試看輸入「功能」\n\nps.\n沒有反應可以再輸入一次或是稍後再試>///<");
     });
     bot.on('follow', function (event) {
         event.source.profile().then(function (profile) {
+            pushUserData(profile);
+            pushActionLog({userId: profile.userId, action: "follow"});
             console.log(profile);
         });
     });
+    // bot.on('unfollow', function (event) {
+    //     event.source.profile().then(function (profile) {
+    //         pushUserData(profile);
+    //         console.log(profile);
+    //     });
+    // });
 }
 
 function _getPosts(url) {
@@ -369,4 +410,87 @@ function reflashToken() {
         })
     // 每10分鐘抓取一次新資料
     timerForToken = setInterval(reflashToken, 600000);
+}
+
+// function getUserData(){
+//     db.ref('/user').once('value', function (snapshot) {
+//         var data = snapshot.val();
+//         userData = [];
+//         for (var item in data) {
+//             userData.push({
+//                 userID: data[item].userId,
+//                 displayName: data[item].displayName,
+//                 pictureUrl: data[item].pictureUrl,
+//                 statusMessage: data[item].statusMessage
+//             });
+//         }
+//         console.log(userData);
+//     });
+// }
+
+function pushUserData(tmp){
+    var noPush = false;
+    db.ref('/user').once('value', function (snapshot) {
+        var data = snapshot.val();
+        for (var item in data) {
+            if (data[item].userId == tmp.userId) {
+                // 如果資料更改需要更新
+                var needUpdate = data[item].displayName != tmp.displayName || data[item].pictureUrl != tmp.pictureUrl || data[item].statusMessage != tmp.statusMessage;
+                if (needUpdate){
+                    db.ref("/user").child(item).set(tmp);
+                }
+                noPush = true;
+            }
+        }
+        // console.log(userData);
+        if (!noPush) {
+            db.ref("/user").push(tmp);
+        }
+    });
+}
+
+function pushActionLog(tmp) {
+    db.ref("/log").push({userId: tmp.userId, action: tmp.action, time: DateTimezone(8)});
+}
+
+function pushGroup(tmp){
+    var noPush = false;
+    db.ref('/group').once('value', function (snapshot) {
+        var data = snapshot.val();
+        for (var item in data) {
+            if (data[item].groupId == tmp) {
+                noPush = true;
+            }
+        }
+        if (!noPush) {
+            db.ref("/group").push({groupId: tmp});
+        }
+    });
+}
+
+function pushRoom(tmp) {
+    var noPush = false;
+    db.ref('/room').once('value', function (snapshot) {
+        var data = snapshot.val();
+        for (var item in data) {
+            if (data[item].roomId == tmp) {
+                noPush = true;
+            }
+        }
+        if (!noPush) {
+            db.ref("/room").push({roomId: tmp});
+        }
+    });
+}
+
+// 新增當地時區的時間物件
+function DateTimezone(offset) {
+    // 建立現在時間的物件
+    d = new Date();
+    // 取得 UTC time
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    // 新增不同時區的日期資料
+    return new Date(utc + (3600000 * offset)).toLocaleString();
+    // 8是台北
+    // DateTimezone(8)
 }
